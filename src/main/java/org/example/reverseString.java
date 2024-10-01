4ec123401e47e113e85e460525688fcd4767556f
Hinga Bernard <hinga.bernardn@gmail.com>
2024-10-01 15:21:05 +0100

    Exercise 2

---
+++ src/main/java/org/example/reverseString.java
@@ -0,0 +1,24 @@
+//Exercise 2
+package org.example;
+
+import java.util.Scanner;
+
+public class reverseString {
+    public static void main(String[] args) {
+//        Initiate the original string with user input.
+        System.out.println("Type a word: ");
+        Scanner sc = new Scanner(System.in);
+        String originalString = sc.nextLine();
+
+//        Variable to store the reverse string.
+        String reversedString = "";
+
+//        Initialize the for loop with the length of the string(-1 to ensure loop isn't out of bounds).
+        for (int i = originalString.length() - 1; i >= 0 ; i--) {
+//            Concatenate each character into the reversed string.
+            reversedString += originalString.charAt(i);
+        }
+
+        System.out.println("Original word: " + originalString + "\nReversed word: " + reversedString);
+    }
+}
