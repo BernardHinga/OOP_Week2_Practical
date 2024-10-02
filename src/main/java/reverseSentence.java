54fbfa9cfa68a7f9bb3ea545e8388fc96765ef00
Hinga Bernard <hinga.bernardn@gmail.com>
2024-10-02 23:05:39 +0100

    Exercise 3

---
+++ src/main/java/reverseSentence.java
@@ -0,0 +1,21 @@
+//Exercise 3
+
+import java.util.Scanner;
+
+public class reverseSentence {
+    public static void main(String[] args) {
+//        Create an original sentence from user input.
+        System.out.println("Type a sentence: ");
+        Scanner sc = new Scanner(System.in);
+        String originalSentence = sc.nextLine();
+
+//        Create a variable to store the reverse of the sentence.
+        String reversedSentence = "";
+
+//        Create a variable that will store the number of words in the sentence.
+        int wordCount = originalSentence.split("").length;
+
+//        Loop through the sentence indexing each word.
+
+    }
+}
