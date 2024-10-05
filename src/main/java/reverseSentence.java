13e2ae6dfc6e69b640eb22756b546c6f633d9bb7
Hinga Bernard <hinga.bernardn@gmail.com>
2024-10-05 01:58:51 +0100

    Exercise 3

--- src/main/java/reverseSentence.java
+++ src/main/java/reverseSentence.java
@@ -9,13 +9,23 @@ public class reverseSentence {
         Scanner sc = new Scanner(System.in);
         String originalSentence = sc.nextLine();
 
+//        Create an array to store the words from the sentence
+        String[] words = originalSentence.split(" ");
+
 //        Create a variable to store the reverse of the sentence.
         String reversedSentence = "";
 
 //        Create a variable that will store the number of words in the sentence.
-        int wordCount = originalSentence.split("").length;
+        int wordCount = words.length;
 
-//        Loop through the sentence indexing each word.
+//        Loop through the sentence indexing each word using the word count as the bounds of the loop.
+//        Take into account that arrays count 1integer less than 'natural' counting.
+        for (int i = wordCount - 1; i >= 0; i--) {
+            reversedSentence += words[i] + " ";
+        }
 
+//        Trim possible extra space at the end.
+        reversedSentence = reversedSentence.substring(0, reversedSentence.length() - 1);
+        System.out.println(reversedSentence);
     }
 }
